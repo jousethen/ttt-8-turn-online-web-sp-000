98047eb6f4c95d84ef0cf91a8d3214c1bd27fa15
Jouse then <jthen93@gmail.com>
2021-02-07 21:35:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,17 +1,17 @@
+def position_taken?(board, index)
+  if board[index] == "" || board [index] == " " || board[index] == nil
+    return FALSE
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
 def valid_move? (board,index)
   if index.between?(0,8) && position_taken?(board, index) == false
     return true
   
   else
     return false
-  end
-end
-
-def position_taken?(board, index)
-  if board[index] == "" || board [index] == " " || board[index] == nil
-    return FALSE
-  elsif board[index] == "X" || board[index] == "O"
-    return true
   end
 end
 
